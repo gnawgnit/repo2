885e82f289cf6639566fd3061bcbe79c225d3973
admin <2057739395@qq.com>
2020-07-21 12:55:34 +0800

    测试而已

--- test.c
+++ test.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(void){
-	puts("this is a test from deepin!!!";
+	FILE *fp = fopen("~/test.txt", "r");
+	if(!fp){
+		exit(1);
+		puts("cann't open this file");
+	}
+	FILE *fp2 = fopen("~/test1.txt", "w");
+	if(!fp2){
+		exit(1);
+		puts("cann't open this file");
+	}
+	char ch;
+	while(ch = getc(fp)!= EOF){
+		putc(ch, stdout);
+	}
+	fclose(fp);
+	fclose(fp2);
+	if(!fp||!fp2)
+		puts("cann't close the files");
+	puts("cann't close the files");
 	return 0;
 }
